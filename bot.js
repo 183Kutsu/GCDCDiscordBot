b9950ba5d9c0825e5389e83447779bf27e1ea499
Riddokun <reed.keenan91@gmail.com>
2019-03-22 20:59:43 -0400

    Version 2.0

--- bot.js
+++ bot.js
@@ -1,60 +1,136 @@
-var Discord = require('discord.io');
-var logger = require('winston');
-var auth = require('./auth.json');
-// Configure logger settings
-logger.remove(logger.transports.Console);
-logger.add(new logger.transports.Console, {
-    colorize: true
+const { Client, RichEmbed } = require('discord.js');
+const characterData = require('./characterData');
+const emojiCharacters = require('./emojiCharacters');
+
+var react = 0;
+var remark = 0;
+var input = "";
+
+// Create an instance of a Discord client
+const client = new Client();
+/**
+ * The ready event is vital, it means that only _after_ this will your bot start reacting to information
+ * received from Discord
+ */
+client.on('ready', () => {
+  console.log('I am ready!');
 });
-logger.level = 'debug';
-// Initialize Discord Bot
-var bot = new Discord.Client({
-   token: auth.token,
-   autorun: true
-});
-bot.on('ready', function (evt) {
-    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
-});
-bot.on('message', function (user, userID, channelID, message, evt) {
-    // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with `!`
-    if (message.substring(0, 1) == '!') {
-        var args = message.substring(1).split(' ');
+
+//Sends a Embedded Message depending on users input
+client.on('message', message => {
+    const embed = new RichEmbed()
+
+    if (message.content.substring(0, 1) === '!') {
+        // We can create embeds using the MessageEmbed constructor
+        // Read more about all that you can do with the constructor
+        // over at https://discord.js.org/#/docs/main/stable/class/RichEmbed
+        var args = message.content.substring(1).split(' ');
         var cmd = args[0];
-
-        var characterSR = ["ELESIS", "ARME", "LIRE", "RYAN", "RONAN", "AMY", "JIN", "LASS", "SIEGHART", "LEY", "RIN", "DIO"];
-        var characterS;
-        var equipment;
+        input = cmd.toUpperCase(); 
         
-       
-        args = args.splice(1);
+        var characterSR = ["ELESIS", "ARME", "LIRE", "RYAN", "RONAN", "AMY", "JIN", "LASS", "SIEGHART", "LEY", "RIN", "DIO", "VEIGAS", "RUFUS"];
 
-        /// This will autopopulate characters
-        /// Only allow GC Channgel and Testing server
-        if(channelID === "531559203005792266" || channelID ==="531389494016868353") {
-            if(characterSR.indexOf(cmd.toUpperCase()) > -1) {
-                bot.sendMessage({
-                    to: channelID,
-                    message: "http://grandchase.wikia.com/wiki/" + cmd + "/Grand_Chase_Dimensional_Chaser#Evolution"
-                });          
-            }
-            else { 
-                bot.sendMessage({
-                    to:channelID,
-                    message: "Too much for me to process! (ﾟAﾟ;)"
+        args = args.splice(1);
+        if(characterSR.indexOf(input) > -1) {
+            //message.channel.sendMessage("http://grandchase.wikia.com/wiki/" + cmd + "/Grand_Chase_Dimensional_Chaser#Evolution");            
+            //Get data from dictionary
+            if(characterData.data[input]) {     
+                embed.setImage(characterData.data[input].image)
+                embed.setTitle(characterData.data[input].name)
+                embed.setColor(0xFF0000)
+                embed.setDescription(
+                    characterData.data[input].name + "\n What would you like to know:" +
+                        "\n1. Recommended Sets\n2. Recommended Accessories\n3. Recommended Traits"
+                );
+                react = 1;           
+
+                //Attach reactions to sent message 
+                message.channel.send(embed).then(async function (message) {
+                    await message.react(emojiCharacters[1])
+                    await message.react(emojiCharacters[2])
+                    //await message.react(emojiCharacters[3])
                 });
             }
-            /*switch(cmd) {
-                /// This will autopopulate characters
-                case 'elesis':
-                    bot.sendMessage({
-                        to: channelID,
-                        message: 'http://grandchase.wikia.com/wiki/Elesis/Grand_Chase_Dimensional_Chaser#Evolution'
-                    });
-                break;
-                }*/
-        }
-     }
+        else { /*Nothing was found */ }
+        }        
+        else { 
+            //embed.setImage(obj.image) //Show soccer chick with the whistle
+            embed.setTitle("Error 404" - input)
+            embed.setColor(0xFF0000)
+            embed.setDescription("Too much for me to process! (ﾟAﾟ;)")
+            react = 0;   
+            message.channel.send(embed);
+        }        
+    }
 });
+
+client.on('messageReactionAdd', (reaction, user) => {
+    if(reaction.emoji.identifier === "1%E2%83%A3" && reaction.count > 1) {
+        // This is for Recommended Sets
+        remark = 1;
+        reaction.message.delete();
+
+        getSets(reaction, characterData.data[input]);
+    } 
+    else if(reaction.emoji.identifier === "2%E2%83%A3" && reaction.count > 1) {
+        // This is for Recommended Accessories
+        remark = 2;
+        reaction.message.delete();
+        
+        getAccessories(reaction, characterData.data[input]);
+    } 
+    else if(reaction.emoji.identifier === "3%E2%83%A3" && reaction.count > 1) {
+        remark = 3;
+        reaction.message.delete();
+    }
+});
+
+function getSets(reaction, data) {
+    for(var value = 1; value <= data.sets.length - 1; value++) {            
+        var getSet = ""; // Get Skills from the Array
+        var getSetIcon = data.sets[value].set[0]; // Get Skills Icon            
+        var getSetTitle = data.sets[value].set[1]; // Get Equip's Title
+        
+        for(var val = 2; val <= data.sets[value].set.length - 1; val++) {
+            getSet += data.sets[value].set[val]; // Get Skills info
+        }
+
+        const embed = new RichEmbed()
+        .setAuthor(getSetTitle, getSetIcon)
+        .setColor(0xFF0000)
+        .setDescription(getSet
+            .replace(/\[/g,"**")
+            .replace(/\]/g,"**")
+        );
+        reaction.message.channel.send(embed);
+    }
+}
+
+function getAccessories(reaction, data) {
+    let numberOfAccessoriesOptions = 0
+    for(var value = 0; value <= data.Accessories.length - 1; value++) {            
+        var getAcc = ""; // Get Skills from the Array
+        var getAccIcon = data.Accessories[value].acc[0]; // Get Skills Icon
+        var getAccTitle = data.Accessories[value].acc[1]; // Get Equip's Title
+        
+        numberOfAccessoriesOptions++; //Iterates the number of Accessories found for a character
+        
+        for(var val = 2; val <= data.Accessories[value].acc.length - 1; val++) {
+            getAcc += data.Accessories[value].acc[val]; // Get Skills info
+             
+            const embed = new RichEmbed()
+            .setTitle(getAccTitle)
+            .setThumbnail(getAccIcon)
+            //.setAuthor(getAccTitle, getAccIcon)
+            .setColor(0xFF0000)
+            .setDescription(getAcc
+                .replace(/\[/g,"**")
+                .replace(/\]/g,"**")
+            );
+            reaction.message.channel.send("Set Option " + numberOfAccessoriesOptions);
+            reaction.message.channel.send(embed);
+        }
+    }
+}
+// Log our bot in using the token from https://discordapp.com/developers/applications/me
+client.login("NTMxMzYxNjgwMDgxNDIwMzAw.DxNGAA.9XoXNX_-0d9RyplPwbcQ_MtYKpk");
---
+++ characterData.js
@@ -0,0 +1,125 @@
+module.exports = {
+    data:
+    {
+        "ELESIS": {
+            name: "Elesis Sieghart", image: "https://vignette.wikia.nocookie.net/grandchase/images/b/b5/Grand_Chase_for_kakao_Elesis_01.png/revision/latest/scale-to-width-down/310?cb=20180911181402",
+            sets: [
+                { "set": ["Color", "Name", "2 set buff", "4 set buff"] },
+                { "set": ["https://vignette.wikia.nocookie.net/grandchase/images/c/cb/Set-pink.png/revision/latest/scale-to-width-down/84?cb=20180905105147", "Spirit of Luck", "Crit Chance 4%", "Every 50 seconds, acquire [Lucky Blow] effect.\n [Lucky Blow]\n The next skill used will activate at max."] },
+                { "set": ["https://vignette.wikia.nocookie.net/grandchase/images/a/a3/Set-blue.png/revision/latest/scale-to-width-down/84?cb=20180815030146", "Power of Anger", "Skill Cooldown Reduction 4%", "When using skills, acquire [Power of Anger] effect. (Can stack up to a max of 10 times)\n [Power of Anger]\n For each stack, Attack Damage increases by 2% and Damage Received reduces by 2%."] }
+            ],
+            Accessories: [
+                { "acc": ["https://vignette.wikia.nocookie.net/grandchase/images/d/d1/Ring-frost.png/revision/latest?cb=20180814045149", "Glacial Ring", "Reduce base attack damage taken."] },
+                { "acc": ["https://vignette.wikia.nocookie.net/grandchase/images/3/3c/Necklace-safeguard.png/revision/latest/scale-to-width-down/84?cb=20180814045054", "Tank's Necklace", "Reduce skill damage taken."] },
+                { "acc": ["https://vignette.wikia.nocookie.net/grandchase/images/1/1c/Earrings-resistance.png/revision/latest/scale-to-width-down/84?cb=20180814044846", "Earrings of Resistance", "Reduce skill damage taken."] }
+            ]
+        },
+        "LIRE": {
+            name: "Lire Eryuell", image: "https://vignette.wikia.nocookie.net/grandchase/images/9/9c/Grand_Chase_for_kakao_Lire_Eryuell_01.png/revision/latest/scale-to-width-down/580?cb=20180911182230",
+            sets: [
+                { "set": ["Color", "Name", "2 set buff", "4 set buff"] },
+                { "set": ["https://vignette.wikia.nocookie.net/grandchase/images/c/ca/Set-green.png/revision/latest/scale-to-width-down/84?cb=20180815030155", "Vows of Violence", "Basic Attack Speed Increase 6.00%"] }
+            ],
+            Accessories: [
+                { "acc": ["https://vignette.wikia.nocookie.net/grandchase/images/c/ce/Ring-wind.png/revision/latest/scale-to-width-down/84?cb=20180814045225", "Gale Ring", "Increase basic attack speed."] },
+                { "acc": ["https://vignette.wikia.nocookie.net/grandchase/images/e/e7/Necklace-lava.png/revision/latest?cb=20180814045036", "Lava Necklace", "Increase critical chance."] },
+                { "acc": ["https://vignette.wikia.nocookie.net/grandchase/images/0/0e/Earrings-tempest.png/revision/latest?cb=20180814044933", "Earrings of Tempest", "Increase basic attack speed."] }
+            ],
+        },
+        "ARME": {
+            name: "Arme Glenstid", image: "https://vignette.wikia.nocookie.net/grandchase/images/1/16/Grand_Chase_for_kakao_Arme_01.png/revision/latest/scale-to-width-down/454?cb=20180911180946",
+            sets: [
+                { "set": ["Color", "Name", "2 set buff", "4 set buff"] },
+                { "set": ["https://vignette.wikia.nocookie.net/grandchase/images/a/a3/Set-blue.png/revision/latest/scale-to-width-down/84?cb=20180815030146", "Power of Anger", "Skill Cooldown Reduction 4.00%", "When using skills, acquire [Power of Anger] effect. (Can stack up to 10 times)\n[Power of Anger]\nFor each stack, Attack Damage increases by 2% and Damage Received reduces by 2%."] },
+                { "set": ["https://vignette.wikia.nocookie.net/grandchase/images/1/1a/Set-orange.png/revision/latest/scale-to-width-down/84?cb=20180815030204", "Dagger of Passion", "Skill Attack Damage Increase 10.00%", "When using skills, acquire [Dagger of Passion] effect. (Can stack up to 10 times)\n[Dagger of Passion]\nCrit Chance increase by 2% per stack."] },
+            ]
+        },
+        "RONAN": {
+            name: "Ronan Erudon", image: "https://vignette.wikia.nocookie.net/grandchase/images/a/aa/Grand_Chase_for_kakao_Ronan_01.png/revision/latest/scale-to-width-down/619?cb=20180912103450",
+            sets: [
+                { "set": ["Color", "Name", "2 set buff", "4 set buff"] },
+                { "set": ["https://vignette.wikia.nocookie.net/grandchase/images/a/a3/Set-blue.png/revision/latest/scale-to-width-down/84?cb=20180815030146", "Power of Anger", "Skill Cooldown Reduction 4.00%", "When using skills, acquire [Power of Anger] effect. (Can stack up to 10 times)\n[Power of Anger]\nFor each stack, Attack Damage increases by 2% and Damage Received reduces by 2%."] },
+                { "set": ["https://vignette.wikia.nocookie.net/grandchase/images/0/09/Set-red.png/revision/latest/scale-to-width-down/84?cb=20180905105127", "Bloody Revenge", "Skill Cooldown Reduction 4.00%", "Upon taking 20% of maximum health as damage within 1 second, acquire [Bloody Revenge] effect. (20-second cooldown)\n[Bloody Revenge]\nRecovers 15% of maximum HP\nIncreases attack damage by 40% for 10 seconds\nBarrier with durability equal to 40% of HP"] }
+            ]
+        },
+        "LASS": {
+            name: "Lass Isolet", image: "https://vignette.wikia.nocookie.net/grandchase/images/2/29/Grand_Chase_for_kakao_Lass_01.png/revision/latest/scale-to-width-down/620?cb=20180911181731",
+            sets: [
+                { "set": ["Color", "Name", "2 set buff", "4 set buff"] },
+                { "set": ["https://vignette.wikia.nocookie.net/grandchase/images/1/1a/Set-orange.png/revision/latest/scale-to-width-down/84?cb=20180815030204", "Dagger of Passion", "Skill Attack Damage Increase 10.00%", "When using skills, acquire [Dagger of Passion] effect. (Can stack up to 10 times)\n[Dagger of Passion]\nCrit Chance increase by 2% per stack."] }
+            ]
+        },
+        "RYAN": {
+            name: "Ryan Woodguard", image: "https://vignette.wikia.nocookie.net/grandchase/images/c/ca/Grand_Chase_for_kakao_Ryan_01.png/revision/latest/scale-to-width-down/620?cb=20180912103804",
+            sets: [
+                { "set": ["Color", "Name", "2 set buff", "4 set buff"] },
+                { "set": ["https://vignette.wikia.nocookie.net/grandchase/images/a/a3/Set-blue.png/revision/latest/scale-to-width-down/84?cb=20180815030146", "Power of Anger", "Skill Cooldown Reduction 4.00%", "When using skills, acquire [Power of Anger] effect. (Can stack up to 10 times)\n[Power of Anger]\nFor each stack, Attack Damage increases by 2% and Damage Received reduces by 2%."] }
+            ]
+        },
+        "RUFUS": {
+            name: "Rufus Wilde", image: "https://vignette.wikia.nocookie.net/grandchase/images/0/0f/Grand_Chase_for_kakao_Rufus_01.png/revision/latest/scale-to-width-down/505?cb=20180912103622",
+            sets: [
+                { "set": ["Color", "Name", "2 set buff", "4 set buff"] },
+                { "set": ["https://vignette.wikia.nocookie.net/grandchase/images/c/ca/Set-green.png/revision/latest/scale-to-width-down/84?cb=20180815030155", "Vows of Violence", "Basic Attack Speed Increase 6.00%"] }
+            ]
+        },
+        "AMY": {
+            name: "Amy Plie", image: "https://vignette.wikia.nocookie.net/grandchase/images/f/f9/Grand_Chase_for_kakao_Amy_01.png/revision/latest/scale-to-width-down/601?cb=20180911171621",
+            sets: [
+                { "set": ["Color", "Name", "2 set buff", "4 set buff"] },
+                { "set": ["https://vignette.wikia.nocookie.net/grandchase/images/a/a3/Set-blue.png/revision/latest/scale-to-width-down/84?cb=20180815030146", "Power of Anger", "Skill Cooldown Reduction 4.00%", "When using skills, acquire [Power of Anger] effect. (Can stack up to 10 times)\n[Power of Anger]\nFor each stack, Attack Damage increases by 2% and Damage Received reduces by 2%."] },
+                { "set": ["https://vignette.wikia.nocookie.net/grandchase/images/c/ca/Set-green.png/revision/latest/scale-to-width-down/84?cb=20180815030155", "Vows of Violence", "Basic Attack Speed Increase 6.00%"] },
+                { "set": ["https://vignette.wikia.nocookie.net/grandchase/images/1/1a/Set-orange.png/revision/latest/scale-to-width-down/84?cb=20180815030204", "Dagger of Passion", "Skill Attack Damage Increase 10.00%", "When using skills, acquire [Dagger of Passion] effect. (Can stack up to 10 times)\n[Dagger of Passion]\nCrit Chance increase by 2% per stack."] },
+                { "set": ["https://vignette.wikia.nocookie.net/grandchase/images/0/09/Set-red.png/revision/latest/scale-to-width-down/84?cb=20180905105127", "Bloody Revenge", "Skill Cooldown Reduction 4.00%", "Upon taking 20% of maximum health as damage within 1 second, acquire [Bloody Revenge] effect. (20-second cooldown)\n[Bloody Revenge]\nRecovers 15% of maximum HP\nIncreases attack damage by 40% for 10 seconds\nBarrier with durability equal to 40% of HP"] }
+            ]
+        },
+        "SIEGHART": {
+            name: "Ercnard Sieghart", image: "https://vignette.wikia.nocookie.net/grandchase/images/f/f3/Grand_Chase_for_kakao_Sieghart_01.png/revision/latest/scale-to-width-down/620?cb=20180912104502",
+            sets: [
+                { "set": ["Color", "Name", "2 set buff", "4 set buff"] },
+                { "set": ["https://vignette.wikia.nocookie.net/grandchase/images/a/a3/Set-blue.png/revision/latest/scale-to-width-down/84?cb=20180815030146", "Power of Anger", "Skill Cooldown Reduction 4.00%", "When using skills, acquire [Power of Anger] effect. (Can stack up to 10 times)\n[Power of Anger]\nFor each stack, Attack Damage increases by 2% and Damage Received reduces by 2%."] },
+                { "set": ["https://vignette.wikia.nocookie.net/grandchase/images/0/09/Set-red.png/revision/latest/scale-to-width-down/84?cb=20180905105127", "Bloody Revenge", "Skill Cooldown Reduction 4.00%", "Upon taking 20% of maximum health as damage within 1 second, acquire [Bloody Revenge] effect. (20-second cooldown)\n[Bloody Revenge]\nRecovers 15% of maximum HP\nIncreases attack damage by 40% for 10 seconds\nBarrier with durability equal to 40% of HP"] }
+            ]
+        },
+        "LEY": {
+            name: "Ley von Crimson River", image: "https://vignette.wikia.nocookie.net/grandchase/images/9/96/Grand_Chase_for_kakao_Ley_01.png/revision/latest/scale-to-width-down/518?cb=20180911181917",
+            sets: [
+                { "set": ["Color", "Name", "2 set buff", "4 set buff"] },
+                { "set": ["https://vignette.wikia.nocookie.net/grandchase/images/c/ca/Set-green.png/revision/latest/scale-to-width-down/84?cb=20180815030155", "Vows of Violence", "Basic Attack Speed Increase 6.00%"] },
+            ]
+        },
+        "JIN": {
+            name: "Jin Kaien", image: "https://vignette.wikia.nocookie.net/grandchase/images/e/e3/Grand_Chase_for_kakao_Jin_01.png/revision/latest/scale-to-width-down/620?cb=20180911181552",
+            sets: [
+                { "set": ["Color", "Name", "2 set buff", "4 set buff"] },
+                { "set": ["https://vignette.wikia.nocookie.net/grandchase/images/a/a3/Set-blue.png/revision/latest/scale-to-width-down/84?cb=20180815030146", "Power of Anger", "Skill Cooldown Reduction 4.00%", "When using skills, acquire [Power of Anger] effect. (Can stack up to 10 times)\n[Power of Anger]\nFor each stack, Attack Damage increases by 2% and Damage Received reduces by 2%."] },
+                { "set": ["https://vignette.wikia.nocookie.net/grandchase/images/1/1a/Set-orange.png/revision/latest/scale-to-width-down/84?cb=20180815030204", "Dagger of Passion", "Skill Attack Damage Increase 10.00%", "When using skills, acquire [Dagger of Passion] effect. (Can stack up to 10 times)\n[Dagger of Passion]\nCrit Chance increase by 2% per stack."] },
+            ]
+        },
+        "DIO": {
+            name: "Dio Burning Canyon", image: "https://vignette.wikia.nocookie.net/grandchase/images/3/36/Grand_Chase_for_kakao_Dio_01.png/revision/latest/scale-to-width-down/500?cb=20180911181232",
+            sets: [
+                { "set": ["Color", "Name", "2 set buff", "4 set buff"] },
+                { "set": ["https://vignette.wikia.nocookie.net/grandchase/images/a/a3/Set-blue.png/revision/latest/scale-to-width-down/84?cb=20180815030146", "Power of Anger", "Skill Cooldown Reduction 4.00%", "When using skills, acquire [Power of Anger] effect. (Can stack up to 10 times)\n[Power of Anger]\nFor each stack, Attack Damage increases by 2% and Damage Received reduces by 2%."] },
+                { "set": ["https://vignette.wikia.nocookie.net/grandchase/images/c/ca/Set-green.png/revision/latest/scale-to-width-down/84?cb=20180815030155", "Vows of Violence", "Basic Attack Speed Increase 6.00%"] },
+                { "set": ["https://vignette.wikia.nocookie.net/grandchase/images/0/09/Set-red.png/revision/latest/scale-to-width-down/84?cb=20180905105127", "Bloody Revenge", "Skill Cooldown Reduction 4.00%", "Upon taking 20% of maximum health as damage within 1 second, acquire [Bloody Revenge] effect. (20-second cooldown)\n[Bloody Revenge]\nRecovers 15% of maximum HP\nIncreases attack damage by 40% for 10 seconds\nBarrier with durability equal to 40% of HP"] }
+            ]
+        },
+        "RIN": {
+            name: "Rin", image: "https://vignette.wikia.nocookie.net/grandchase/images/c/c3/Grand_Chase_for_kakao_Rin_01.png/revision/latest/scale-to-width-down/512?cb=20180912103226",
+            sets: [
+                { "set": ["Color", "Name", "2 set buff", "4 set buff"] },
+                { "set": ["https://vignette.wikiax.nocookie.net/grandchase/images/a/a3/Set-blue.png/revision/latest/scale-to-width-down/84?cb=20180815030146", "Power of Anger", "Skill Cooldown Reduction 4.00%", "When using skills, acquire [Power of Anger] effect. (Can stack up to 10 times)\n[Power of Anger]\nFor each stack, Attack Damage increases by 2% and Damage Received reduces by 2%."] },
+                { "set": ["https://vignette.wikia.nocookie.net/grandchase/images/1/1a/Set-orange.png/revision/latest/scale-to-width-down/84?cb=20180815030204", "Dagger of Passion", "Skill Attack Damage Increase 10.00%", "When using skills, acquire [Dagger of Passion] effect. (Can stack up to 10 times)\n[Dagger of Passion]\nCrit Chance increase by 2% per stack."] }
+            ]
+        },
+        "VEIGAS": {
+            name: "Veigas Terre", image: "https://vignette.wikia.nocookie.net/grandchase/images/6/64/Grand_Chase_for_kakao_Veigas_01.png/revision/latest/scale-to-width-down/620?cb=20180912104738",
+            sets: [
+                { "set": ["Color", "Name", "2 set buff", "4 set buff"] },
+                { "set": ["https://vignette.wikia.nocookie.net/grandchase/images/a/a3/Set-blue.png/revision/latest/scale-to-width-down/84?cb=20180815030146", "Power of Anger", "Skill Cooldown Reduction 4.00%", "When using skills, acquire [Power of Anger] effect. (Can stack up to 10 times)\n[Power of Anger]\nFor each stack, Attack Damage increases by 2% and Damage Received reduces by 2%."] },
+                { "set": ["https://vignette.wikia.nocookie.net/grandchase/images/1/1a/Set-orange.png/revision/latest/scale-to-width-down/84?cb=20180815030204", "Dagger of Passion", "Skill Attack Damage Increase 10.00%", "When using skills, acquire [Dagger of Passion] effect. (Can stack up to 10 times)\n[Dagger of Passion]\nCrit Chance increase by 2% per stack."] }
+            ]
+        }
+    }
+};
